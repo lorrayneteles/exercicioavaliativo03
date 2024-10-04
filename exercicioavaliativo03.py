06218f4a2d60f5762cbb2744bc2f33549b787054
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 20:52:58 -0300

    Add files via upload

---
+++ exercicioavaliativo03.py
@@ -0,0 +1,7 @@
+# Peça ao usuario dois numeros e mostre a multiplicaçao  deles
+n1 = int(input("Digite o primeiro número:\n"))
+n2 = int(input("Digite o segundo número:\n"))
+
+multiplicacao = ( n1 * n2)
+
+print(" A multiplicação de dois números é:\n", multiplicacao)
